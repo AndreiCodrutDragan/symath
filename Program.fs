7cf25c7ffd050f1da2c5abe555ea169fd8b4557b
Matei Ionut-Eduard <50235164+MateiIonutEduard@users.noreply.github.com>
2022-06-28 22:03:16 +0300

    Add base operators

--- Program.fs
+++ Program.fs
@@ -11,3 +11,20 @@
     | Log of Expression
     | Sin of Expression
     | Cos of Expression
+
+let (|Op|_|) (x : Expression) =
+    match x with
+    | Add(e1, e2) -> Some(Add, e1, e2)
+    | Sub(e1, e2) -> Some(Sub, e1, e2)
+    | Mul(e1, e2) -> Some(Mul, e1, e2)
+    | Div(e1, e2) -> Some(Div, e1, e2)
+    | Pow(e1, e2) -> Some(Pow, e1, e2)
+    | _ -> None
+
+let (|Func|_|) (x : Expression) =
+    match x with
+    | Exp(e) -> Some(Exp, e)
+    | Log(e) -> Some(Log, e)
+    | Sin(e) -> Some(Sin, e)
+    | Cos(e) -> Some(Cos, e)
+    | _ -> None
