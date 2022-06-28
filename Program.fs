cdc416283978ae286f4f91d0d8823256b01b9419
Andu <Andu@DESKTOP-BKOH64B>
2022-06-28 21:42:53 +0300

    Add project files.

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿type Expression =
+    | X
+    | Const of float
+    | Neg of Expression
+    | Add of Expression * Expression
+    | Sub of Expression * Expression
+    | Mul of Expression * Expression
+    | Div of Expression * Expression
+    | Pow of Expression * Expression
+    | Exp of Expression
+    | Log of Expression
+    | Sin of Expression
+    | Cos of Expression
